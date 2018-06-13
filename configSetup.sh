6232f3b6400121914743d94dbea1b3eb771ace53
David Vogelbacher <davidvogelbacher@gmx.de>
2018-06-12 20:39:30 -0400

    refactor

--- configSetup.sh
+++ configSetup.sh
@@ -1,26 +1,25 @@
-#!/bin/bash
+#!/bin/zsh
 
 
 print() {
   echo -e "\033[1;31m$1\033[0m"
 }
-zsh
 print "Changing default shell to zsh"
 chsh -s /usr/bin/zsh
 
 
 print "Copying .zsrhc"
-cp configs/.zshrc ~/.zshrc
+cp config/.zshrc ~/.zshrc
 
 print "Reloading .zsrhc"
 source ~/.zshrc
 
 print "Copying i3 config"
-cp configs/.i3/config ~/.config/i3/config
+cp config/i3/config ~/.config/i3/config
 
 print "Reloading i3"
 i3-msg reload
 
 print "Reloading terminal config"
-dconf load /org/gnome/terminal/ < gnome-terminal.dconf
+dconf load /org/gnome/terminal/ < config/gnome-terminal.dconf
 
